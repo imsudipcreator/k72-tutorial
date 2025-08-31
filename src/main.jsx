8806842a27d174f4f98ee5cda997daf1d847679d
imsudipcreator <imsudipmahata@gmail.com>
2025-08-31 14:10:27 +0530

    initiated react router dom for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+import { createRoot } from "react-dom/client";
+import Projects from "./pages/Projects.jsx";
+import Agency from "./pages/Agency.jsx";
+import { StrictMode } from "react";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/agency" element={<Agency />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
